655ce249e0845490adf2ed7fbe55184544c39d16
old-programmer <ybauer53@gmail.com>
2017-07-13 13:42:27 +0300

    Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 ﻿class PostsController < ApplicationController
+
+	before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+
 	def index	
 		@posts = Post.all	
 	end
 	
 	def show	
-		@post = Post.find(params[:id])	
 	end
 
 	def new
@@ -21,11 +23,9 @@
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update_attributes(post_params)
 			redirect_to @post
 		else
@@ -40,6 +40,10 @@
 	end
 
 	private 
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params 
 		params.require(:post).permit(:title, :summary, :body)	
