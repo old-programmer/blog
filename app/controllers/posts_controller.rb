a42eff716907db2f5fa08638b3fb6e9374468da1
old-programmer <ybauer53@gmail.com>
2017-07-13 12:56:32 +0300

    Post: edit, update, destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,25 @@
 		end
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_params)
+			redirect_to @post
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
 	private 
 
 	def post_params 
