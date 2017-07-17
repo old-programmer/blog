e0cf9cbb4beca47cce6939ca8095f87472df21d0
old-programmer <ybauer53@gmail.com>
2017-07-17 12:27:10 +0300

    Add model Picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714221817) do
+ActiveRecord::Schema.define(version: 20170716072352) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
