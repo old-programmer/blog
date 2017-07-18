ab1f1dd2e7f968a763cc2068c76e6e7c040cc027
old-programmer <ybauer53@gmail.com>
2017-07-18 21:51:24 +0300

    Add Category (Model && Controller)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,6 +48,6 @@
 	end
 
 	def post_params 
-		params.require(:post).permit(:title, :summary, :body, :image, :all_tags)	
+		params.require(:post).permit(:title, :summary, :body, :image, :all_tags, :category_id)	
 	end 
 end
